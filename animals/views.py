6b8007800d95d8f1aa881d68e2219dac24eff7e3
Elizabeth <mykhailenkoyelyzaveta@gmail.com>
2025-05-11 16:42:36 +0300

    +main, +new_animal

--- animals/views.py
+++ animals/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def main(request):
+    return HttpResponse("<h1> Я їду в подорож </h1>")
+
+def new_animal(request):
+    return HttpResponse("<ul> <li> Я бачила рибку дорі </li> </ul>")
